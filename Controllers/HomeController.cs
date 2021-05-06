e1000a3ab6ec6db62d7018de86b346cc1d60aa6c
Alonso Gutelius <alonso_gutelius@usmp.pe>
2021-05-06 11:21:59 -0500

    prueba

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -27,6 +27,10 @@ namespace huertodelvalle.Controllers
         {
             return View();
         }
+        public IActionResult prueba2()
+        {
+            return View();
+        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
